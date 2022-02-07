6c488c5f6a80370df5ad0e58c8cba56187c824ae
shivanshisharma <43888903+shivanshisharma@users.noreply.github.com>
2022-02-06 21:11:34 -0700

    Add files via upload

---
+++ SumCalculator.js
@@ -0,0 +1,5 @@
+var num1 = prompt("Please enter the first number");
+var num2 = prompt("Please enter the second number");
+alert("You have entered: " + num1 + " and " + num2);
+alert("The sum is " + (Number(num1)+Number(num2)));
+
